a7ab7172f86d34d5418bc94f93c1906c1d967980
Meenu Lakshmi <meenulakshmi1617@gmail.com>
2025-04-02 08:49:03 +0530

    Create hashes.sh

---
+++ hashes.sh
@@ -0,0 +1,22 @@
+
+#!/bin/bash
+
+hash_db="hashes.txt" 
+
+create_hashes() {
+    sha256sum * > "$hash_db"
+}
+
+check_files() {
+    sha256sum -c "$hash_db" 
+}
+
+case $1 in
+    create) create_hashes ;;
+    check)  check_files ;;
+esac
+
+
+# ./5d.sh create     # Generate hashes
+# ./5d.sh check      # Verify file integrity
+# hashes.txt is output file
